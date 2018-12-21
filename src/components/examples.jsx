070b592461cfff567a0b3804f3f7920e80534958
DESKTOP-IQE7QK3\jonas <jonasrdsantos@gmail.com>
2018-12-20 23:37:09 -0200

    Simplified assembly of the page

--- src/components/examples.jsx
+++ src/components/examples.jsx
@@ -14,48 +14,26 @@ import ClassesExample from "./examples/classesExample";
 class Examples extends Component {
   state = {};
   render() {
+    const showExample = function(code, name, Component) {
+      return (
+        <div>
+          <ExampleHeader title={`Example ${code} - ${name}`} />
+          <Component />
+        </div>
+      );
+    };
     return (
       <React.Fragment>
-        <div>
-          <ExampleHeader title="Example 1 - Hello World" />
-          <HelloWorld />
-        </div>
-        <div>
-          <ExampleHeader title="Example 2 - Let vs Var vs Const" />
-          <LetVarConst />
-        </div>
-        <div>
-          <ExampleHeader title="Example 3 - Objects" />
-          <ObjectsExample />
-        </div>
-        <div>
-          <ExampleHeader title="Example 4 - This Keyword" />
-          <ThisKeyword />
-        </div>
-        <div>
-          <ExampleHeader title="Example 5 - Arrow Function" />
-          <ArrowFunction />
-        </div>
-        <div>
-          <ExampleHeader title="Example 6 - Arrow Function and this" />
-          <ArrowFunctionAndThis />
-        </div>
-        <div>
-          <ExampleHeader title="Example 7 - ArrayMap" />
-          <ArrayMapExample />
-        </div>
-        <div>
-          <ExampleHeader title="Example 8 - Object destructuring" />
-          <ObjectDesctructuringExample />
-        </div>
-        <div>
-          <ExampleHeader title="Example 9 - Spread operator" />
-          <SpreadOperatorExample />
-        </div>
-        <div>
-          <ExampleHeader title="Example 10 - Classes" />
-          <ClassesExample />
-        </div>
+        {showExample(1, "Hello World", HelloWorld)}
+        {showExample(2, "Let vs Var vs Const", LetVarConst)}
+        {showExample(3, "Objects", ObjectsExample)}
+        {showExample(4, "This Keyword", ThisKeyword)}
+        {showExample(5, "Arrow Function", ArrowFunction)}
+        {showExample(6, "Arrow Function and this", ArrowFunctionAndThis)}
+        {showExample(7, "ArrayMap", ArrayMapExample)}
+        {showExample(8, "Object destructuring", ObjectDesctructuringExample)}
+        {showExample(9, "Spread operator", SpreadOperatorExample)}
+        {showExample(10, "Classes and modules", ClassesExample)}
       </React.Fragment>
     );
   }
